e0e51ac29ff8fdd5d3681cf6e5239e9588d25f37
rajkumarpawar07 <pawarrajkumar0707@gmail.com>
2025-06-12 13:52:06 +0530

    pushed

--- service1/app/main.py
+++ service1/app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Service 1!", 200  # Change to Service 2 for second service
+    return "Hello World from Service 1!", 200  # Change to Service 2 for second service
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
